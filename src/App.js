f69808f2c15ac787e0a2bdcbbf10430faabd0261
Mahesh Pandekar <pmahesh6789@gmail.com>
2020-12-11 20:11:47 +0100

    Initial routing added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import react from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Nav from './components/navigations/Nav';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h4>Welcome to my demo.</h4>
+      <Router>
+        <Nav/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/navigations/Nav.js
@@ -0,0 +1,22 @@
+import react from "react";
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
