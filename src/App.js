98a2c842b04515e9bba0029929560c3d653d53fa
Mahesh Pandekar <pmahesh6789@gmail.com>
2020-12-11 19:51:22 +0100

    Initial application setup completed.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Welcome to my demo.</h4>
+    </div>
+  );
+}
+
+export default App;
